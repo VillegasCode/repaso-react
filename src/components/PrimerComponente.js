08b36d858b9b2b13f25710d0a703c6bb8982f936
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-24 13:14:41 -0500

    Button to change name with hook useState through destructuring of the variable

--- src/components/PrimerComponente.js
+++ src/components/PrimerComponente.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const PrimerComponente = () => {
   
-    let nombre = "Carlos Villegas";
+    // let nombre = "Carlos Villegas";
     let web = "empresarioencasa.org";
 
     // Creamos un array
@@ -12,11 +12,23 @@ export const PrimerComponente = () => {
       "Master en React",
       "Master en CSS"
     ];
+
+    //Desestructurar una variable para cambiarle su estado con HOOK useState
+    const [nombre, setNombre] = useState("Alex");
+
+    //Creamos una función para cambiar nombre con el HOOK useState
+    const cambiarNombre = (nuevoNombre) =>{
+      setNombre(nuevoNombre);
+    }
   
     return (
     <div>
         <h1>PRIMER COMPONENTE</h1>
         <p>Mi nombre es: {nombre} </p>
+
+        <input type='text' onChange={e => cambiarNombre(e.target.value)} placeholder='Escribe un nuevo nombre'></input>
+
+        <button onClick={ e => cambiarNombre("Evita Bonita")}>Cambiar Nombre</button>
         <p>Mi web es: {web} </p>
         <hr></hr>
         <h2>CURSOS:</h2>
