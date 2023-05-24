f9e316f94cd91975f1f365a0e52b9dd950dd8358
Carlos Villegas <carlos.villegasch@gmail.com>
2023-05-24 12:48:12 -0500

    Create an array and iterate with .map method and a index to array curso

--- src/components/PrimerComponente.js
+++ src/components/PrimerComponente.js
@@ -4,12 +4,33 @@ export const PrimerComponente = () => {
   
     let nombre = "Carlos Villegas";
     let web = "empresarioencasa.org";
+
+    // Creamos un array
+    let cursos = [
+      "Master en Javascript",
+      "Master en PHP",
+      "Master en React",
+      "Master en CSS"
+    ];
   
     return (
-    <div>PrimerComponente
+    <div>
         <h1>PRIMER COMPONENTE</h1>
         <p>Mi nombre es: {nombre} </p>
         <p>Mi web es: {web} </p>
+        <hr></hr>
+        <h2>CURSOS:</h2>
+        <ol>
+          {
+            cursos.map( (curso, indice) => {
+              return (<li key={indice}>
+                {
+                  curso
+                }
+              </li>);
+            })
+          }
+        </ol>
     </div>
   )
 }
